981240dcfd3a74192d3cb4c4da7951ad5663a686
José Cama La Madrid <111785479+JoseMegun@users.noreply.github.com>
2024-06-14 10:34:14 -0500

    Mejoras de Front-End con SonarCloud

--- src/app/moderation/moderation-list/moderation-list.component.spec.ts
+++ src/app/moderation/moderation-list/moderation-list.component.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ModerationListComponent } from './moderation-list.component';
+import { ModerationService } from 'src/app/services/moderation.service';
 
 describe('ModerationListComponent', () => {
   let component: ModerationListComponent;
   let fixture: ComponentFixture<ModerationListComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ModerationListComponent],
+      providers: [ModerationService]
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ModerationListComponent]
-    });
     fixture = TestBed.createComponent(ModerationListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,3 +25,4 @@ describe('ModerationListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
--- src/app/services/moderation.service.spec.ts
+++ src/app/services/moderation.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ModerationService } from './moderation.service';
 
 describe('ModerationService', () => {
   let service: ModerationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ModerationService]
+    });
     service = TestBed.inject(ModerationService);
   });
 
